47deb7af11c3c7728b604a38b312577e7c7cab00
knktc <me@knktc.com>
2018-09-09 21:35:20 +0800

    finsh url in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django_qingstor_storage',
     version='0.1',
     packages=['django_qingstor_storage'],
-    url='https://git./knktc/django-qingstor-storage',
+    url='https://github.com/knktc/django-qingstor-storage',
     license='Apache License 2.0',
     author='knktc',
     author_email='me@knktc.com',

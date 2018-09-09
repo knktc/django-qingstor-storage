533b77723c32f6807c6944d292f1a0427666f59d
knktc <me@knktc.com>
2018-09-09 22:24:12 +0800

    modify setup.py and readme, now you can install from pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='django_qingstor_storage',
-    version='0.1',
+    version='0.1.1',
     packages=['django_qingstor_storage'],
     url='https://github.com/knktc/django-qingstor-storage',
-    license='Apache License 2.0',
     author='knktc',
     author_email='me@knktc.com',
     description='Django storage with Qingstor',
-    install_requires=['django>=2.0', 'qingstor-sdk>=2.2.6', ]
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['django>=2.0', 'qingstor-sdk>=2.2.6', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+    ],
 )
